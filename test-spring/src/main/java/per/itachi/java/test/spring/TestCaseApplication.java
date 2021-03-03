1d9580d58bcf568de6547686a13d067ca0a1bd28
ryougiitachi-hm <ryougiitachi@hotmail.com>
2021-03-03 23:58:30 +0800

    spring: improved test case.

---
+++ test-spring/src/main/java/per/itachi/java/test/spring/TestCaseApplication.java
@@ -0,0 +1,11 @@
+package per.itachi.java.test.spring;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCaseApplication.class, args);
+	}
+
+}
--- test-spring/src/test/java/per/itachi/java/test/spring/service/impl/OrderServiceImplTest.java
+++ test-spring/src/test/java/per/itachi/java/test/spring/service/impl/OrderServiceImplTest.java
@@ -1,22 +1,40 @@
 package per.itachi.java.test.spring.service.impl;
 
+import java.util.Collections;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import per.itachi.java.test.spring.TestCaseConfiguration;
+import per.itachi.java.test.spring.configuration.MvcConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = TestCaseConfiguration.class)
+@SpringBootTest(classes = MvcConfiguration.class)
 public class OrderServiceImplTest {
 	
 	private final Logger logger = LoggerFactory.getLogger(OrderServiceImplTest.class);
 	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@Test
-	public void test() {
+	public void test() throws JsonProcessingException {
 		logger.info("This is a test method. ");
+		logger.info("json is {}. ", objectMapper.writeValueAsString(Collections.singletonMap("key", "value")));
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("https://www.baidu.com", Object.class);
 	}
+	
 }
